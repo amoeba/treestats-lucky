b2bfdae6c0a54a6be90ce95c0f1f44e2c0d793a8
Bryce Mecum <petridish@gmail.com>
2017-12-26 22:18:51 -0900

    Fix PG validation errors

--- db/migrations/20171224173412_create_character.cr
+++ db/migrations/20171224173412_create_character.cr
@@ -3,9 +3,9 @@ class CreateCharacter::V20171224173412 < LuckyMigrator::Migration::V1
     create :characters do
      add name : String
      add server : String
-     add level : Int32
-     add race : Int32
-     add gender : Int32
+     add level : Int32?
+     add race : Int32?
+     add gender : Int32?
     end
   end
 
