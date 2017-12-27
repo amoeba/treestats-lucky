24c3232a308cbb65aec7caa55d66f36a3996ad83
Bryce Mecum <petridish@gmail.com>
2017-12-26 22:19:12 -0900

    Make character uploading come from the request body

--- src/actions/characters/create.cr
+++ src/actions/characters/create.cr
@@ -2,25 +2,22 @@ require "json"
 
 class Characters::Create < BrowserAction
   action do
-    data = %({
-        "name": "Test",
-        "server": "Test",
-        "level": 275,
-        "race": 0,
-        "gender": 0
-    })
+    # data = %({
+    #     "name": "Test",
+    #     "server": "Test",
+    #     "level": 275,
+    #     "race": 0,
+    #     "gender": 0
+    # })
+    data = request.body.to_s
+    params = NamedTuple(name: String, server: String).from(JSON.parse(data).as_h)
 
-    pp data
+    saved = CharacterForm.new(**params).save
 
-    parsed = JSON.parse(data)
-    pp parsed
-
-    nt = NamedTuple.from_json(parsed)
-    pp nt
-
-    # character = CharacterForm.new(**JSON.parse(data)).save
-    # pp character
-
-    render_text "Done"
+    if saved
+      render_text "#{saved}"
+    else
+      render_text "#{saved}"
+    end
   end
 end
