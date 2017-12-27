8494b589ecffc6723f10b1281b657f9ccd17a626
Bryce Mecum <petridish@gmail.com>
2017-12-26 20:45:20 -0900

    Make optional character attributes nilable

--- src/models/character.cr
+++ src/models/character.cr
@@ -2,8 +2,8 @@ class Character < BaseModel
   table :characters do
     field name : String
     field server : String
-    field level : Int32
-    field race : Int32
-    field gender : Int32
+    field level : Int32?
+    field race : Int32?
+    field gender : Int32?
   end
 end
